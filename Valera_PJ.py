99e72342741ffc81de8261dd3c2439c414254851
VvlademirG <113224130+VvlademirG@users.noreply.github.com>
2023-04-14 12:10:59 -0700

    Add files via upload

---
+++ Valera_PJ.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# ## Hello World!!!
+
+# ##### This Notebook Val used for summarized. This is introduction.
+# 
+# 
+
+# # Popular languages:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala
+# 5. Java
+# 6. C++
+# 7. Julia
+# 8. JavaScript
+# 9. PHP
+# 10. Go
+# 11. Ruby
+# 12. Visual Basic
+
+# ### Data science libraries:
+# 
+# 1.   Pandas
+# 
+# 2.   Numpy
+# 
+# 3.   Matplotlib
+# 
+# 4.   Seaborn
+# 
+# 5.   Scikit-learn
+# 
+# 6.   Keras
+# 
+# 7.   TensorFlow
+# 
+# 8.   PyTorch
+# 
+# 9.   Apache Spark
+# 
+# 10.  Vegas
+# 
+# 11.  Big DL
+# 
+# 12.  Ggplot2
+
+# |  Data Science tools | Name | 
+# | :- | -: | 
+# |1|R Studuo 
+# |2|Jyputer
+# |3|Anacomda
+
+# #### introductory sentence about arithmetic expression examples
+
+# In[1]:
+
+
+#  arithmetic examples
+(5*5)+5-13
+
+
+# In[2]:
+
+
+#  arithmetic examples
+200//60
+
+
+# ## Objectives:
+#  - Add comments
+#  - create markdowm
+#  - Add Order
+#  - Format comtent 
+
+# ## Author
+# #### Valerii Goriatov
+
+# In[ ]:
+
+
+
+
